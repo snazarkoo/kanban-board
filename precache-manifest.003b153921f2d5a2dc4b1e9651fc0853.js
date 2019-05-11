e3e6cb26437297a35a4c8a3988919af4263ac506
Nazar Shcherbiak <nazar.shcherbiak@c-map.com>
2019-05-11 13:57:49 +0300

    Updates

---
+++ precache-manifest.003b153921f2d5a2dc4b1e9651fc0853.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60041fd7f8ebc41035a2471527c02c5b",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "8a7125feaafdae3d4074",
+    "url": "/kanban-board/static/js/2.ebaa888c.chunk.js"
+  },
+  {
+    "revision": "2545b29c8e3eeeacae7b",
+    "url": "/kanban-board/static/js/main.dc4e1d5e.chunk.js"
+  },
+  {
+    "revision": "2a97cbc7210d9a44b96f",
+    "url": "/kanban-board/static/js/runtime~main.8507f43b.js"
+  }
+]);
